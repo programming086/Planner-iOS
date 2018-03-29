c706bdae9c2479cd38606f88a4562d06a5d9003f
Roman Brovko <brovko-roman@ya.ru>
2018-03-29 21:52:38 +0300

    lesson 5

--- Planner/Controllers/ViewController.swift
+++ Planner/Controllers/ViewController.swift
@@ -3,18 +3,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // вызывается после инициализации
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // симулятор загрузки формы (чтобы успеть посмотреть launchscreen) - в рабочем проекте естественно нужно будет удалить
-        for i in 0...700000 {
+        for i in 0...100000 {
             print(i)
         }
-
-
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // вызывается, если не хватает памяти (чтобы очистить ресурсы)
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
